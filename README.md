163a1e6336a0fb62235f13f4bb7e244794cf4cd7
Albert Chang <a89529294@hotmail.com>
2023-01-14 16:32:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-use-please-stay
+
+A fun React hook that animates the document title and favicon when focus from the page is lost.
---
+++ dist/usePleaseStay.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.usePleaseStay = void 0;
+function usePleaseStay() { }
+exports.usePleaseStay = usePleaseStay;
